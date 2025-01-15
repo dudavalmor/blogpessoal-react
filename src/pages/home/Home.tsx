7a7f4e20c8d44edb59f701381a34a82db5ab540b
Eduarda Valmor <eduarda.bserrano@gmail.com>
2025-01-15 20:12:19 -0300

    tailwind no projeto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,33 +1,18 @@
-
 function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
+            <div className="w-screen flex justify-center">
                 <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <h2>Seja Bem Vinde e se sinta acolhide!</h2>
+                    <div className="max-w-7xl flex flex-col items-center">
+                    <h2>Seja Bem Vinde e se sinta acolhide!</h2>
                         <p>Expresse aqui seus pensamentos e opniões</p>
                         <p>♥ Feito por Eduarda Valmor ♥</p>
                     </div>
 
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <img 
-                            src="https://i.imgur.com/VpwApCU.png" 
-                            alt="Imagem da Página Home" 
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
                             width="400px"
                         />
                     </div>
