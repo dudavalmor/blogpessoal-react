97131582f08c3c329faf4afd281cdc47ab51863d
Eduarda Valmor <eduarda.bserrano@gmail.com>
2025-01-24 22:30:31 -0300

    entrega final

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blog-pessoal-69my.onrender.com/'
+    baseURL: 'https://blog-pessoal-cif1.onrender.com'
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
